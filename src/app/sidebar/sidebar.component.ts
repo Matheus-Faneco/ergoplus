a48425eeea4308659588cffbb13713de09ea8e6e
Matheus Faneco <matheusfaneco2009@hotmail.com>
2024-12-06 21:41:08 -0400

    remoção de alguns códigos e alteraçoes no CSS

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -6,23 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 
-//NÃO ESTÁ FUNCIONANDO COMO DEVERIA
 export class SidebarComponent {
-  activeMenu: string | null = null;
   openSubMenu: string | null = null;
 
-  //setActiveMenu marca qual a opção de aba está marcada
-  //activeMenu armazena a string que é classe da aba que está selecionada
-  //Era a partir da aba ativa, que poderia ser usado como referência para manter esta aba azul.
-  //As funções estão OK, deve ser algo no CSS
-  setActiveMenu(menuName: string) {
-    if (this.activeMenu === menuName) {
-      this.activeMenu = null;
-    } else {
-      this.activeMenu = menuName;
-    }
-    this.openSubMenu = null;
-  }
   //toggleSubMenu verifica se a aba "Relatório" está aberto ou não, se não estiver, ele aciona a abertura via Event Binding (click no HTML).
   //Foi implementado pra fazer a abertura do submenu da aba "Relatório"
   toggleSubMenu(subMenuName: string) {
