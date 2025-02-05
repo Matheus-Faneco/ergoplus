0435a883c736e9136b0799df950d76fd9d2c0f73
Nicolas <nicolassan2015@gmail.com>
2025-02-04 23:19:44 -0400

    Configurando as cameras

--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -7,16 +7,45 @@ import { Component } from '@angular/core';
 })
 export class MonitorComponent {
 
-  async startCamera() {
+  async startCamera(cameraNumber: number) {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        video: true});
+      const devices = await navigator.mediaDevices.enumerateDevices();
+      const videoDevices = devices.filter(device => device.kind === 'videoinput');
 
-      const videoELement = document.getElementById('camera-preview') as HTMLVideoElement;
-      videoELement.srcObject = stream;
-      videoELement.play();
+      if (videoDevices.length < 2) {
+        console.error("Menos de duas câmeras disponíveis.");
+        return;
+      }
+
+      console.log("Dispositivos de vídeo encontrados:", videoDevices);
+
+      let stream: MediaStream | null = null;
+
+      if (cameraNumber === 1) {
+        stream = await navigator.mediaDevices.getUserMedia({
+          video: { deviceId: videoDevices[0].deviceId }
+        });
+
+        const videoElement1 = document.getElementById('camera-preview-1') as HTMLVideoElement;
+        if (videoElement1.srcObject !== stream) {
+          videoElement1.srcObject = stream;
+          videoElement1.play();
+        }
+
+      } else if (cameraNumber === 2) {
+        stream = await navigator.mediaDevices.getUserMedia({
+          video: { deviceId: videoDevices[1].deviceId }
+        });
+
+        const videoElement2 = document.getElementById('camera-preview-2') as HTMLVideoElement;
+        if (videoElement2.srcObject !== stream) {
+          videoElement2.srcObject = stream;
+          videoElement2.play();
+        }
+      }
+
     } catch (error) {
-      console.error("erro ao acessar a camera:", error);
+      console.error("Erro ao acessar as câmeras:", error);
     }
   }
 
